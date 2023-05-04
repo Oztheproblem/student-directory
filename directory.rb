9cb601f86006801f6373e83d98e7f7161a81b4c9
Osman Khan <osman_khan1987@hotmail.com>
2023-05-04 23:10:06 +0100

    refactored step 10

--- directory.rb
+++ directory.rb
@@ -1,26 +1,7 @@
-# let's put all students into hashes
-=begin
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-  ]
-=end
-
-
 def print_header
-    puts "The Students of Villains Academy"
-    puts "----------"
+  puts "The Students of Villains Academy"
+  puts "----------"
 end
-
 
 def input_students
   puts "Please enter the names of the students"
@@ -41,19 +22,47 @@ def input_students
   students
 end
 
-=begin
-def print(students)
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  end
-=end
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
-#nothing happens until we call the methods
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+interactive_menu
